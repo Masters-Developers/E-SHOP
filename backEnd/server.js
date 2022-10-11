c59080b21804b4de8b71d4155c8039e69cb44aef
AndyGreyrat <charles.florez@udea.edu.co>
2022-10-11 18:54:27 -0500

    Update server.js

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,4 +1,6 @@
 const app=require("./app")
+// const express = require("express")
+// const app = express()
 const connectDatabase = require("./config/database.js");
 
 //Set the config file
@@ -10,5 +12,7 @@ connectDatabase();
 
 //CALLING SERVER
 const server=app.listen(process.env.PORT, () => {
-    console.log(`Server started in PORT: ${process.env.PORT} in mode: ${process.env.NODE_ENV}`)
+    console.log(`Server started in port: ${process.env.PORT} in mode: ${process.env.NODE_ENV}`)
 })
+// app.listen(4000)
+// console.log(`server in port: ${4000}`)
