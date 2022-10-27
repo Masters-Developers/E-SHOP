e45e42a41b05938500fa4e0f66ee24088d3ba4cb
AndyGreyrat <charles.florez@udea.edu.co>
2022-10-27 11:41:24 -0500

    Update itemsController.js

--- backEnd/controllers/itemsController.js
+++ backEnd/controllers/itemsController.js
@@ -1,78 +1,71 @@
 
-const items=require("../models/items")
+const itemss=require("../models/items");
+const ErrorHandler = require("../utilities/errorHandler");
+const catchAsyncErrors = require ("../middleWare/catchAsyncErrors")
 const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url)); //Import Fetch
-
 //VIEW ITEMS'S LIST
-exports.getItems= async(req,res,next) =>{
-    const itemsList = await items.find();
-    res.status(200).json({
-        success:true,
-        amount: items.length,
-        itemsList
-    })
-}
-//VIEW PRODUCTS BY ID
-exports.getItemsByID=async(req,res,next) =>{
-    const itemId = await items.findById(req.params.id);
-
-    if(!itemId){
-        return res.status(404).json({
-            success:false,
-            message:"Item's Id not found"
-        }) 
+exports.getItems= catchAsyncErrors( async(req,res,next) =>{
+    const items = await itemss.find();
+    if(!items){
+        return next(new ErrorHandler ("Items not found", 404))
     }
     res.status(200).json({
         success:true,
-        message:"below you can find the informacion wbout the item",
-        itemId
+        amount: items.length,
+        items
     })
-}
-//Create new item /api/items
-exports.newItem=async(req,res,next)=>{
-    const item = await items.create(req.body);
+})
+//VIEW PRODUCTS BY ID
+exports.getItemsByID= catchAsyncErrors( async(req,res,next) =>{
+    const item = await itemss.findById(req.params.id);
 
+    if(!item){
+        return next(new ErrorHandler ("Item not found", 404))
+    }
+    res.status(200).json({
+        success:true,
+        message:"below you can find the details about this item: ",
+        item
+    })
+})
+//Create new item /api/items
+exports.newItem= catchAsyncErrors( async(req,res,next)=>{
+    const item = await itemss.create(req.body);
     res.status(201).json({
         success:true,
         item
     })
-}
+})
 //Update item
-exports.updateItem=async(req,res,next)=>{
-    let itemId = await items.findById(req.params.id);
+exports.updateItem=catchAsyncErrors( async(req,res,next)=>{
+    let item = await itemss.findById(req.params.id);
 
-    if(!itemId){
-        return res.status(404).json({
-            success:false,
-            message:"Item's Id not found"
-        }) 
+    if(!item){
+        return next(new ErrorHandler ("Item's Id not found", 404))
     }
-    const item = await items.findByIdAndUpdate(req.params.id,req.body,{
+    item = await itemss.findByIdAndUpdate(req.params.id,req.body,{
         new:true, 
         runValidators:true
     })
     res.status(200).json({
         success:true,
         message:"Succesfully updated Item",
-        itemId
+        item
     })
-
-}
+})
 //DELETE A ITEM
-exports.deleteItem= async (req,res,next) =>{
-    const itemId= await items.findById(req.params.id) //
-    if (!itemId){ //
-            return res.status(404).json({ //
-            success:false,
-            message: 'Item not found'
-        })
+exports.deleteItem=catchAsyncErrors( async (req,res,next) =>{
+    const item= await itemss.findById(req.params.id) //
+    if(!item){
+        return next(new ErrorHandler ("Item not found", 404))
     }
 
-    await itemId.remove();//delete the process
+    await item.remove();//delete the process
     res.status(200).json({
         success:true,
         message:"Succesfully deleted Item"
     })
-}
+})
 //view all items
 function viewItems(){
     fetch('http://localhost:4000/api/items')
